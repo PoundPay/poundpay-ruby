0c386caa66509199c28612647817b0c5d84fbc68
Matin Tamizi <mtamizi@gmail.com>
2011-02-11 12:43:09 -0800

    full test coverage of Poundpay::Resource

--- spec/poundpay/resource_spec.rb
+++ spec/poundpay/resource_spec.rb
@@ -6,28 +6,48 @@ class FakeElement < Resource
 end
 
 describe Resource do
-  describe "#primary_key" do
+  describe "#self.primary_key" do
     it "should default to 'sid'" do
       Resource.primary_key.should == "sid"
     end
   end
 
-  describe "#element_path" do
+  describe "#self.element_path" do
     it "should not add a extension to the element_path" do
       FakeElement.element_path(1).should == "/version/fake_elements/1"
-      FakeElement.element_path("FExxxx").should == "/version/fake_elements/FExxxx"
     end
   end
 
-  describe "#new_element_path" do
+  describe "#self.new_element_path" do
     it "should not add a extension to the new_element_path" do
       FakeElement.new_element_path.should == "/version/fake_elements/new"
     end
   end
 
-  describe "#collection_path" do
+  describe "#self.collection_path" do
     it "should not add a extension to the collection_path" do
       FakeElement.collection_path.should == "/version/fake_elements"
+    end
+  end
+
+  describe "#self.instantiate_collection" do
+    it "should handle paginated responses" do
+      collection = {"fake_elements" => [{"sid" => 1}, {"sid" => 2}]}
+      fake_elements = FakeElement.instantiate_collection(collection)
+      fake_elements.should == [FakeElement.new(:sid => 1), FakeElement.new(:sid => 2)]
+    end
+  end
+
+  describe "#encode" do
+    it "should urlencode the attributes" do
+      fake_element = FakeElement.new(:foo => "bar baz")
+      fake_element.encode.should == "foo=bar+baz"
+    end
+  end
+
+  describe "#collection_name" do
+    it "should get the collection_name" do
+      FakeElement.new.collection_name.should == "fake_elements"
     end
   end
 end
