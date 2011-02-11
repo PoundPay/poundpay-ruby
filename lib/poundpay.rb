a092ec23d1687546a520128790a8e549a8ccae34
Matin Tamizi <mtamizi@gmail.com>
2011-02-11 10:31:29 -0800

    initial spec tests

--- lib/poundpay.rb
+++ lib/poundpay.rb
@@ -5,10 +5,14 @@ module Poundpay
   VERSION = "silver"
 
   class << self
-    def configure(sid, token, api_url=API_URL, version=VERSION)
+    def configure(developer_sid, auth_token, api_url=API_URL, version=VERSION)
+      unless developer_sid.start_with? "DV"
+        raise ArgumentError.new "developer_sid should start with DV.  Make sure " \
+          "you're using the right developer_sid"
+      end
       Resource.site = "#{api_url}/#{version}/"
-      Resource.user = sid
-      Resource.password = token
+      Resource.user = developer_sid
+      Resource.password = auth_token
     end
   end
 
--- spec/poundpay_spec.rb
+++ spec/poundpay_spec.rb
@@ -2,6 +2,16 @@ require 'poundpay'
 
 describe Poundpay do
   describe "#configure" do
+    it "should require developer_sid to start with DV" do
+      expect {
+        # Pass developer_sid and auth_token in wrong order
+        Poundpay.configure(
+          "c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae1540200449cc4526b2381a",
+          "DV0383d447360511e0bbac00264a09ff3c",
+        )
+      }.to raise_error(ArgumentError, /DV/)
+    end
+
     it "should be configured with default api_url and version" do
       developer_sid = "DV0383d447360511e0bbac00264a09ff3c"
       auth_token = "c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae1540200449cc4526b2381a"
@@ -9,6 +19,16 @@ describe Poundpay do
       Poundpay::Resource.user.should == developer_sid
       Poundpay::Resource.password.should == auth_token
       Poundpay::Resource.site.to_s.should == "https://api.poundpay.com/silver/"
+    end
+
+    it "should accept optional api_url and version" do
+      Poundpay.configure(
+        "DV0383d447360511e0bbac00264a09ff3c",
+        "c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae1540200449cc4526b2381a",
+        api_url="https://api-sandbox.poundpay.com",
+        version="gold",
+      )
+      Poundpay::Resource.site.to_s.should == "https://api-sandbox.poundpay.com/gold/"
     end
   end
 end
