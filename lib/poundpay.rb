ebee975f4f2d42283762771624e6615725f08aaa
Matin Tamizi <mtamizi@gmail.com>
2011-02-10 19:28:14 -0800

    activepound -> poundpay

--- lib/poundpay.rb
+++ lib/poundpay.rb
@@ -1,125 +1,84 @@
-# @author PoundPay
-module PoundPay
-  require 'net/http'
-  require 'net/https'
-  require 'uri'
-  require 'cgi'
-  require 'json'
+require 'cgi'
+
+require 'active_resource'
+require 'active_resource/formats'
 
 
-  API_URL = 'https://api.poundpay.com'
-  VERSION = 'silver'
+module ActiveResource
+  module Formats
+    module UrlencodedJsonFormat
+      extend self
 
-
-  class PoundPayClient
-    attr_reader :sid
-      
-    #initialize a poundpay account object
-    #
-    #@param [String, String] Your PoundPay Acount SID and Auth Token
-    #@return [Object] PoundPay account object
-    def initialize(sid, token, api_url=API_URL, version=VERSION)
-      @sid = sid
-      @token = token
-      @api_url = api_url
-      @version = version
-    end
-    
-    #sends a request and gets a response from the PoundPay REST API
-    #
-    #@param [String, String, Hash]
-    #path, the URL (relative to the endpoint URL, after the /v1
-    #method, the HTTP method to use, defaults to POST
-    #vars, for POST or PUT, a dict of data to send
-    #
-    #@return PoundPay response
-    #@raises [ArgumentError] Invalid path parameter
-    #@raises [NotImplementedError] Method given is not implemented
-    def request(path, method=nil, vars={})
-      if !path || path.length < 1
-          raise ArgumentError, 'Invalid path parameter'
-      end
-      if method && !['GET', 'POST', 'DELETE', 'PUT'].include?(method)
-        raise NotImplementedError, 'HTTP %s not implemented' % method
+      def extension
+        "json"
       end
 
-      if path[0, 1] != '/'
-        path = '/' + path
+      def mime_type
+        "application/x-www-form-urlencoded"
       end
-      uri = base_url + path
-      return fetch(uri, vars, method)
-    end
-    
-    #enocde the parameters into a URL friendly string
-    #
-    #@param [Hash] URL key / values        
-    #@return [String] Encoded URL
-    protected
-    def base_url
-      return "#{@api_url}/#{@version}"
-    end
 
-    def urlencode(params)
-      params.to_a.collect! \
-        { |k, v| "#{k}=#{CGI.escape(v.to_s)}" }.join("&")
-    end
-    
-    # Create the uri for the REST call
-    #
-    #@param [String, Hash] Base URL and URL parameters        
-    #@return [String] URI for the REST call
-    def build_get_uri(uri, params)
-      if params && params.length > 0
-        if uri.include?('?')
-          if uri[-1, 1] != '&'
-            uri += '&'
-          end
-            uri += urlencode(params)
-          else
-            uri += '?' + urlencode(params)
-        end
+      def decode(json)
+        ActiveSupport::JSON.decode(json)
       end
-      return uri
-    end
-    
-    # Returns a http request for the given url and parameters
-    #
-    #@param [String, Hash, String] Base URL, URL parameters, optional METHOD        
-    #@return [String] URI for the REST call
-    def fetch(url, params, method=nil)
-      if method && method == 'GET'
-        url = build_get_uri(url, params)
-      end
-      uri = URI.parse(url)
-      
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      
-      if method && method == 'GET'
-        req = Net::HTTP::Get.new(uri.request_uri)
-      elsif method && method == 'DELETE'
-        req = Net::HTTP::Delete.new(uri.request_uri)
-      elsif method && method == 'PUT'
-        req = Net::HTTP::Put.new(uri.request_uri)
-        req.set_form_data(params)
-      else
-        req = Net::HTTP::Post.new(uri.request_uri)
-        req.set_form_data(params)
-      end
-      req.basic_auth(@sid, @token)
-      
-      return http.request(req)
     end
   end
 end
 
 
-# Patch HTTPResponse to add .json
-class Net::HTTPResponse
-  def json
-    if not @json
-      @json = JSON.load @body
+module Poundpay
+  class PoundpayResource < ActiveResource::Base
+    self.site = "http://localhost:8000/silver/"
+    self.user = "DV8539761e250011e0a81d1231400042c7"
+    self.password = "5d291d63059f5c2fe8d144820a847982a9fba7004a1009a35ed7a5fdf1f67960"
+    self.format = ActiveResource::Formats::UrlencodedJsonFormat
+
+    class << self
+      attr_accessor_with_default(:primary_key, 'sid')
+
+      def element_path(id, prefix_options = {}, query_options = nil)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{collection_name}/#{URI.escape id.to_s}#{query_string(query_options)}"
+      end
+
+      def new_element_path(prefix_options = {})
+        "#{prefix(prefix_options)}#{collection_name}/new"
+      end
+
+      def collection_path(prefix_options = {}, query_options = nil)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
+      end
+
+      def instantiate_collection(collection, prefix_options = {})
+        # TODO: Consume pages
+        collection = collection[collection_name]
+        super(collection, prefix_options)
+      end
     end
-    return @json
+
+    def encode
+      @attributes.to_a.collect { |k, v| "#{k}=#{CGI.escape(v.to_s)}" }.join("&")
+    end
+
+    def collection_name
+      self.class.collection_name
+    end
+  end
+
+
+  class Developer < PoundpayResource
+    class << self
+      def me
+        find(self.user)
+      end
+    end
+  end
+
+
+  class Payment < PoundpayResource
+    def release
+      self.status = 'RELEASED'
+      self.save
+    end
   end
 end
