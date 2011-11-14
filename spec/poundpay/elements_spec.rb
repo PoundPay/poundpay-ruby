b51d83cb4c6e940a6a63868bcc4106b3fcea9f0d
Andrew <andrew@poundpay.com>
2011-11-13 18:54:40 -0800

    - revert partial release - add charge permissions - add developer charge permission callback url

--- spec/poundpay/elements_spec.rb
+++ spec/poundpay/elements_spec.rb
@@ -2,6 +2,7 @@ require 'poundpay'
 require 'poundpay/elements'
 require 'fixtures/payments'
 require 'fixtures/developers'
+require 'fixtures/charge_permissions'
 
 include Poundpay
 
@@ -94,44 +95,7 @@ describe Payment do
       @escrowed_payment.release
       @escrowed_payment.status.should == 'RELEASED'
     end
-
-    it "should release a PARTIALLY_RELEASED payment" do
-      @escrowed_payment = Payment.new partially_released_payment_attributes
-      @escrowed_payment.should_receive(:save).and_return(Payment.new partially_released_payment_attributes)
-
-      @escrowed_payment.release
-      @escrowed_payment.status.should == 'RELEASED'
-    end
-  end
-
-  describe "#partially_release" do
-    it "should not be able to partially release a STAGED payment" do
-      @staged_payment = Payment.new staged_payment_attributes
-      expect {@staged_payment.release}.to raise_error(PaymentReleaseException)
-    end
-
-    it "should partially release an ESCROWED payment" do
-      @escrowed_payment = Payment.new escrowed_payment_attributes
-      @escrowed_payment.should_receive(:save).and_return(Payment.new released_payment_attributes)
-
-      @escrowed_payment.partially_release(123)
-      @escrowed_payment.status.should == 'PARTIALLY_RELEASED'
-    end
-
-    it "should partially release a PARTIALLY_RELEASED payment" do
-      @escrowed_payment = Payment.new partially_released_payment_attributes
-      @escrowed_payment.should_receive(:save).and_return(Payment.new partially_released_payment_attributes)
-
-      @escrowed_payment.partially_release(123)
-      @escrowed_payment.status.should == 'PARTIALLY_RELEASED'
-    end
-
-    it "should not be able to partially release a RELEASED payment" do
-      @staged_payment = Payment.new staged_payment_attributes
-      expect {@staged_payment.partially_release(123)}.to raise_error(PaymentPartiallyReleaseException)
-    end
-  end
-  
+  end  
 
   describe "#cancel" do
     it "should not be able to cancel a STAGED payment" do
@@ -146,13 +110,40 @@ describe Payment do
       @escrowed_payment.cancel
       @escrowed_payment.status.should == 'CANCELED'
     end
-    
-    it "should cancel a PARTIALLY_RELEASED payment" do
-      @escrowed_payment = Payment.new partially_released_payment_attributes
-      @escrowed_payment.should_receive(:save).and_return(Payment.new partially_released_payment_attributes)
+  end
+end
 
-      @escrowed_payment.cancel
-      @escrowed_payment.status.should == 'CANCELED'
+describe ChargePermission do
+  include ChargePermissionFixture
+
+  before (:all) do
+    Poundpay.configure(
+      "DV0383d447360511e0bbac00264a09ff3c",
+      "c31155b9f944d7aed204bdb2a253fef13b4fdcc6ae1540200449cc4526b2381a")
+  end
+
+  after (:all) do
+    Poundpay.clear_config!
+  end
+
+  describe "#deactivate" do
+    it "should not be able to deactivate an INACTIVE charge permission" do
+      @inactive_charge_permission = ChargePermission.new inactive_attributes
+      expect {@inactive_charge_permission.deactivate}.to raise_error(ChargePermissionDeactivateException)
+    end
+
+    it "should deactivate a CREATED charge permission" do
+      @created_charge_permission = ChargePermission.new created_attributes
+      @created_charge_permission.should_receive(:save).and_return(ChargePermission.new created_attributes)
+      @created_charge_permission.deactivate
+      @created_charge_permission.state.should == 'INACTIVE'
+    end
+    
+    it "should deactivate an ACTIVE charge permission" do
+      @active_charge_permission = ChargePermission.new active_attributes
+      @active_charge_permission.should_receive(:save).and_return(ChargePermission.new active_attributes)
+      @active_charge_permission.deactivate
+      @active_charge_permission.state.should == 'INACTIVE'
     end
   end
 end
