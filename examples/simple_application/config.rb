74b8e20f2d9b1602a8f662b306e20d3243c3d641
Mahmoud Abdelkader <mabdelkader@gmail.com>
2011-11-18 12:38:50 -0800

    migrate config back to sandbox

--- examples/simple_application/config.rb
+++ examples/simple_application/config.rb
@@ -1,8 +1,8 @@
 class SimpleApplication
   CONFIG = {
     poundpay: {
-      "api_url" => "http://localhost:8000",
-      "www_url" => "http://localhost:5000",
+      "api_url" => "https://api-sandbox.poundpay.com",
+      "www_url" => "https://www-sandbox.poundpay.com",
       "version" => "silver",
       "developer_sid" => "DV2f8a5168710c11e0aab3123140005921",
       "auth_token" => "f309a5f600a630f7293c64783eaebc8b67a34428cfcd96e82599657a12769924",
