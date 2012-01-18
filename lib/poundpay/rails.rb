fe46dbd79c78563741365f230cb580eb29311d2b
Brian Racer <bracer@gmail.com>
2012-01-18 15:37:29 -0700

    allow erb in poundpay.yml

--- lib/poundpay/rails.rb
+++ lib/poundpay/rails.rb
@@ -3,7 +3,7 @@ if defined? Rails
     def self.configure_from_yaml(path)
       pathname = Pathname.new Rails.root.join(path)
       raise ArgumentError.new "File does not exist: #{pathname.to_s}" unless pathname.exist?
-      config = YAML::load_file(pathname)[Rails.env]
+      config = YAML::load(ERB.new(File.read(pathname)).result)[Rails.env]
       Poundpay.configure_from_hash(config)
     end
   end
